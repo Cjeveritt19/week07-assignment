0a7d3aef1a1389749ed0eeebe81d1a0c2e5f9bf4
Cjeveritt19 <callumeveritt11@gmail.com>
2025-09-25 15:01:46 +0100

    added a templete for this weeks assignment in the server and client

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import Content from "./components/Content.jsx";
+import Form from "./components/Form.jsx";
+
+export default function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>;
+    </>
+  );
+}
