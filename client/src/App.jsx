2303c8b32e158843d82f51c7969379b12299a733
Cjeveritt19 <callumeveritt11@gmail.com>
2025-09-25 16:03:32 +0100

    added some notes to everything, included a Planning readme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import Content from "./components/Content.jsx";
 import Form from "./components/Form.jsx";
 
+// TODO: We set up routes in here. (Routes, Route)
+//- All our components will be rendered in here
+
 export default function App() {
   return (
     <>
