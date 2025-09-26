f311755fc055db65f4999f5da65bf9ce4120b3f8
Cjeveritt19 <callumeveritt11@gmail.com>
2025-09-26 16:18:50 +0100

    tried adding the api for a league table but had no luck with it, added styling and some more code to the home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,23 +1,36 @@
-//TODO: render the database data sent through the server
-// - store data in state
-// - fetch the values from the server --> this is an effect
-// - once you fetch teh data, parse it to JSON --> console log it!
-// - update state with the fetched data (maybe some wrangling?)
-// - poll your database to keep your data updated --> this is an effect
-// - render the list of content
-//- Stretch: you could conditionally render some elements in your page
+import "./Home.css";
 export default function Home() {
   return (
     <>
-      <h1 className="Home">Welcome to the Liverpool FC community webpage</h1>
-      <p>
-        On this page you can see recent news regarding Liverpool Fc and the
-        latest injuries.
-      </p>
-      <p>
-        You can also see the current league table and post your own comments
-        regarding the team.
-      </p>
+      <div className="Container">
+        <h1 className="Home">Welcome to the Liverpool FC community webpage</h1>
+        <p>
+          On this page you can see recent news regarding Liverpool Fc and the
+          latest injuries.
+        </p>
+        <p>
+          You can also see the current league table and post your own comments
+          regarding the team.
+        </p>
+        <div className="Content">
+          <div className="News">
+            <h2>Recent news:</h2>
+          </div>
+          <div className="Injuries">
+            <h2>Injuries and Suspensions:</h2>
+            <ul>
+              <li>
+                Stefan Bajcetic: Hamstring injury. Expected return: Unknown
+              </li>
+              <li>
+                Giovanni Leoni: Crucate Ligament injury. Expected return: Late
+                december 2025
+              </li>
+              <li>Hugo Ekitike: Suspended.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- client/src/components/LeagueTable.jsx
+++ client/src/components/LeagueTable.jsx
@@ -4,6 +4,7 @@ export default function League() {
   return (
     <>
       <h1 className="League">League Table</h1>
+      <p>COMING SOON!</p>
     </>
   );
 }
